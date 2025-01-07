8a8913e839ed82c253db3e7e20fe7dd577241df6
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-07 09:36:00 -0500

    coded first part of adding new task - next need to do patch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
+  # must place tasks/new first bc of rails flow - it has the static path over tasks/:id
+  get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: :task
 end
